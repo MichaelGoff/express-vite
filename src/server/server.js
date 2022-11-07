f057a211fdc0f039ba81509850eeb5f55c3f92cf
Michael Goff <michael@goffdrop.com>
2022-11-07 17:44:40 -0500

    vite dev server

--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import fs from 'node:fs';
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
+import {createServer as createViteServer} from 'vite';
 
 
 
@@ -11,12 +12,41 @@ export async function createServer(root = process.cwd(), isProd = process.env.NO
     const __dirname = path.dirname(fileURLToPath(import.meta.url))
     const resolve = (p) => path.resolve(__dirname, p)
 
-    app.use('*', async (req, res) => {
-        const template = fs.readFileSync(resolve('../../index.html'), 'utf-8')
+    // Create Vite server in middleware mode and configure the app type as
+    // 'custom', disabling Vite's own HTML serving logic so parent server
+    // can take control
+    const vite = await createViteServer({
+        server: { middlewareMode: true },
+        appType: 'custom'
+    })
 
-        const html = template;
+    // use vite's connect instance as middleware
+    // if you use your own express router (express.Router()), you should use router.use
+    app.use(vite.middlewares)
 
-        res.status(200).set({'Content-Type': 'text/html'}).end(html);
+    app.use('*', async (req, res, next) => {
+        const url = req.originalUrl;
+
+        try {
+            let template = fs.readFileSync(resolve('../../index.html'), 'utf-8');
+
+            // 2. Apply Vite HTML transforms. This injects the Vite HMR client, and
+            //    also applies HTML transforms from Vite plugins, e.g. global preambles
+            //    from @vitejs/plugin-react
+            template = await vite.transformIndexHtml(url, template)
+
+            const html = template;
+
+
+            // In theory insert nonce and other junk here?
+
+            res.status(200).set({'Content-Type': 'text/html'}).end(html);
+        } catch (e) {
+            // If an error is caught, let Vite fix the stack trace so it maps back to
+            // your actual source code.
+            vite.ssrFixStacktrace(e)
+            next(e)
+        }
     });
 
     return {app};
