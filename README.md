ddaadc91bbb14514c89392f93c2157b0d7623238
Michael Goff <michael@goffdrop.com>
2022-11-08 07:59:56 -0500

    Serve up prod

---
+++ README.md
@@ -0,0 +1,6 @@
+# Express Server + Vite in Middleware Mode
+
+Sample repo that serves up a Vue 2.7 project from an express server. Dev server uses Vite in middleware mode. In production, run a vite build and serve up bundle assets instead. No Vite required. 
+
+* [Vite Vue SSR Playground](https://github.com/vitejs/vite/blob/main/playground/ssr-vue/server.js)
+* [Vite in Middleware mode with server-side rendering](https://vitejs.dev/guide/ssr.html#server-side-rendering)
--- src/server/server.js
+++ src/server/server.js
@@ -2,49 +2,59 @@ import express from 'express';
 import fs from 'node:fs';
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
-import {createServer as createViteServer} from 'vite';
-
-
 
 export async function createServer(root = process.cwd(), isProd = process.env.NODE_ENV === 'production', hmrPort) {
-    const app = express();
-
     const __dirname = path.dirname(fileURLToPath(import.meta.url))
     const resolve = (p) => path.resolve(__dirname, p)
+    
+    // Preload index file if we're in production mode. No Vite necessary
+    const indexProd = isProd ? fs.readFileSync(resolve('../../dist/index.html')) : '';
+    
+    const app = express();
+    let vite;
 
-    // Create Vite server in middleware mode and configure the app type as
-    // 'custom', disabling Vite's own HTML serving logic so parent server
-    // can take control
-    const vite = await createViteServer({
-        server: { middlewareMode: true },
-        appType: 'custom'
-    })
+    // Vite is only used in development mode. Production mode serves up built assets
+    if (!isProd) {
+        // Create Vite server in middleware mode and configure the app type as
+        // 'custom', disabling Vite's own HTML serving logic so parent server
+        // can take control
+        vite = await (await import('vite')).createServer({
+            server: { middlewareMode: true },
+            appType: 'custom'
+        })
 
-    // use vite's connect instance as middleware
-    // if you use your own express router (express.Router()), you should use router.use
-    app.use(vite.middlewares)
+        // use vite's connect instance as middleware
+        // if you use your own express router (express.Router()), you should use router.use
+        app.use(vite.middlewares)
+    }
 
     app.use('*', async (req, res, next) => {
         const url = req.originalUrl;
 
         try {
-            let template = fs.readFileSync(resolve('../../index.html'), 'utf-8');
 
-            // 2. Apply Vite HTML transforms. This injects the Vite HMR client, and
-            //    also applies HTML transforms from Vite plugins, e.g. global preambles
-            //    from @vitejs/plugin-react
-            template = await vite.transformIndexHtml(url, template)
+            let template;
+
+            if (!isProd) {
+                template = fs.readFileSync(resolve('../../index.html'), 'utf-8');
+                // 2. Apply Vite HTML transforms. This injects the Vite HMR client, and
+                //    also applies HTML transforms from Vite plugins, e.g. global preambles
+                //    from @vitejs/plugin-react
+                template = await vite.transformIndexHtml(url, template)
+            } else {
+                // Just use the pre-built one in production mode
+                template = indexProd;
+            }
 
             const html = template;
-
-
             // In theory insert nonce and other junk here?
+            // .replace(`<!--some placeholder-->`, nonce?)
 
             res.status(200).set({'Content-Type': 'text/html'}).end(html);
         } catch (e) {
             // If an error is caught, let Vite fix the stack trace so it maps back to
             // your actual source code.
-            vite.ssrFixStacktrace(e)
+            vite && vite.ssrFixStacktrace(e)
             next(e)
         }
     });
